9810f7d52786a5d0a1dfb057248fa1f0a4d5b6c2
mrfuu04 <mrfuu04@gmail.com>
2022-03-26 13:35:15 +0300

    lesson_2

---
+++ mainapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from mainapp.views import index, products
+
+app_name = 'mainapp'
+urlpatterns = [
+    path('', index, name='index'),
+    path('products/', products, name='products')
+]
