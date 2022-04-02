7b6c755e402a9bba90b351319e5e7e745198d76c
mrfuu04 <mrfuu04@gmail.com>
2022-04-02 17:58:57 +0300

    created authentication and corrected html files

--- authapp/views.py
+++ authapp/views.py
@@ -1,13 +1,40 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from authapp.forms import UserLoginForm
+
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('mainapp:index'))
+            else:
+                print('User inactive')
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Авторизация',
+        'form': form,
+    }
+
+    return render(request, 'authapp/login.html', context=context)
 
 
 def register(request):
-    pass
+    return render(request, 'authapp/register.html')
 
 
 def logout(request):
